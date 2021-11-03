30fcc2cf5f73aee942863d31141ce654d5691e86
Einhart <einhvas@gmail.com>
2021-11-04 02:45:51 +0300

    saved store for an app

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,10 @@
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '51796150fdmshcbf0eb18ef13887p1d1ca5jsnbcbd33cb0247'
+//     }
+//   };
+
