b00664fc8bd373b3e4202d62156c4edb166013a1
Einhart <einhvas@gmail.com>
2021-11-04 21:37:41 +0300

    wrote simple api request handling

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,3 +1,23 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '51796150fdmshcbf0eb18ef13887p1d1ca5jsnbcbd33cb0247'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: ()=>createRequest('/exchanges')
+        })
+    })
+});
 
 // var options = {
 //     method: 'GET',
