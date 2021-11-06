8bc62a21b7f8ae91e9d20f6ed5fbb6803304565e
Einhart <einhvas@gmail.com>
2021-11-07 02:05:30 +0300

    Added cryptonewsapihandling

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -23,12 +23,3 @@ export const {
     useGetCryptosQuery,
 } = cryptoApi;
 
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': '51796150fdmshcbf0eb18ef13887p1d1ca5jsnbcbd33cb0247'
-//     }
-//   };
-
